a405719ecddb16bf563de84c7a2f1444706c5a58
IbrahimAbotteen <hemaucla@gmail.com>
2020-09-20 10:30:23 -0700

    running the migrations

--- booklizer2/db/migrate/20200920170515_create_books.rb
+++ booklizer2/db/migrate/20200920170515_create_books.rb
@@ -1,6 +1,10 @@
 class CreateBooks < ActiveRecord::Migration[6.0]
   def change
     create_table :books do |t|
+      t.string :title
+      t.float :price
+      t.integer :subject_id
+      t.text :description
 
       t.timestamps
     end
--- booklizer2/db/migrate/20200920170557_create_subjects.rb
+++ booklizer2/db/migrate/20200920170557_create_subjects.rb
@@ -1,8 +1,13 @@
 class CreateSubjects < ActiveRecord::Migration[6.0]
   def change
     create_table :subjects do |t|
-
+      t.string :name
       t.timestamps
     end
+    Subject.create :name => "Physics"
+    Subject.create :name => "Mathematics"
+    Subject.create :name => "Chemistry"
+    Subject.create :name => "Psychology"
+    Subject.create :name => "Geography"
   end
 end
